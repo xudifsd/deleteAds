cf4bb5febf2375cfc14e8c5bafd27749cecaa0f0
Xu Di <xudifsd@gmail.com>
2011-06-06 17:00:41 +0800

    add commonDelete.js to implement common use

---
+++ commonDelete.js
@@ -0,0 +1,29 @@
+jQuery.noConflict();
+
+jQuery(document).ready(function(){
+	//close button prototype
+	var jQueryhideButton = jQuery('<button/>').wrapInner('X').attr({
+		'class' : 'hideButton',
+		'position' : 'absolute',
+		'top' : '0px',
+		'right' : '0px',
+		}).hide();
+
+	//add button
+	jQuery('div').each(function(){
+		jQuery(this).append(jQueryhideButton.clone());
+	});
+
+	//hide and show button on div
+	jQuery('div').mouseover(function(){
+		jQuery(this).children('button.hideButton').show();
+	}).mouseout(function(){
+		jQuery(this).children('button.hideButton').hide();
+	});
+
+	//click on button event
+	jQuery('button.hideButton').click(function(){
+		jQuery(this).parent().hide();
+	});
+	alert('deleteAds added successfully!');
+});
